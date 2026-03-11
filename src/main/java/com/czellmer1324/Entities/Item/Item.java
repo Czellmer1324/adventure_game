ff4c71cf4124e2e98762a5cacf4ab8c07100cab5
Czellmer1324 <114022227+Czellmer1324@users.noreply.github.com>
2026-03-11 18:41:06 -0400

    added basic structure to Item abstract class

--- src/main/java/com/czellmer1324/Entities/Item/Item.java
+++ src/main/java/com/czellmer1324/Entities/Item/Item.java
@@ -1,4 +1,25 @@
 package com.czellmer1324.Entities.Item;
 
-public interface Item {
+public abstract class Item {
+    private final String name;
+    private final String description;
+    private final ItemType type;
+
+    public Item(String name, String description, ItemType type) {
+        this.name = name;
+        this.description = description;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
 }
--- src/main/java/com/czellmer1324/Entities/Item/ItemType.java
+++ src/main/java/com/czellmer1324/Entities/Item/ItemType.java
@@ -1,4 +1,7 @@
 package com.czellmer1324.Entities.Item;
 
 public enum ItemType {
+    POTION,
+    WEAPON,
+    ARMOR
 }
