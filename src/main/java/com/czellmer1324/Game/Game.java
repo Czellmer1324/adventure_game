6338a5afdc1a2b01c62e0f5ac23dbbf3bfd5d229
Czellmer1324 <114022227+Czellmer1324@users.noreply.github.com>
2026-03-14 15:28:47 -0400

    Updated Game class to not contain game logic

--- src/main/java/com/czellmer1324/Game/Game.java
+++ src/main/java/com/czellmer1324/Game/Game.java
@@ -4,17 +4,38 @@ import com.czellmer1324.Entities.Player.Player;
 import com.czellmer1324.Room.Room;
 import com.czellmer1324.Room.RoomGraph;
 
+import java.util.ArrayList;
+
 public class Game {
     private final RoomGraph gameMap;
-    private Room currentRoom;
+    private final Room startingRoom;
     private final Player player;
     private boolean finished = false;
 
     public Game(String playerName) {
         gameMap = new CreateMap().createMap();
-        currentRoom = gameMap.getStartingRoom();
+        startingRoom = gameMap.getStartingRoom();
         player = new Player(playerName);
     }
 
+    public Room getStartingRoom() {
+        return startingRoom;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setFinished() {
+        finished = true;
+    }
+
+    public boolean gameFinished() {
+        return finished;
+    }
+
+    public ArrayList<Room> getAdjacentRooms(Room room) {
+        return gameMap.getAdjacentRooms(room);
+    }
 
 }
